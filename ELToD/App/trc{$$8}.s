0a39150074bb180488e1a9def1863e1233279e3a
Amar <amarsarvepalli19@gmail.com>
2017-10-04 14:55:23 -0400

    Integrated Toll Policy (I-4 uses old policy and BLW uses new policy)

--- ELToD/App/trc{$$8}.s
+++ ELToD/App/trc{$$8}.s
@@ -1,183 +1,24 @@
-; Script for program NETWORK in file "D:\ELToD v2\ELToD Test w Jack 7.15.16\ELToD_Model_v212_edited_7.16.16\APP\01NET00B.S"
+; Script for program NETWORK in file "M:\Projects\BLW I-4 impacts ELToDv2.3 2017\APP\00NET00Q.S"
 ;;<<Default Template>><<NETWORK>><<Default>>;;
 ; Do not change filenames or add or remove FILEI/FILEO statements using an editor. Use Cube/Application Manager.
-RUN PGM=NETWORK MSG='Network for Assignment'
-FILEI LINKI[5] = "{Directional Link File}",
-VAR=A,B,DIR1
-FILEI LINKI[4] = "{Cap Spd Link File}",
-VAR=A,B,HRCAP,FFSPD
-FILEI LINKI[3] = "{Express Lane Link File}",
-VAR=A,B,FTYPE
-FILEI LINKI[2] = "{Toll Link File}",
-VAR=A,B,TOLLSEGNUM,TOLLSEGLEN,MINSEGTOLL
-FILEO NETO = "{SCENARIO_DIR}\Assign_Input_Net_1.NET",
-INCLUDE = TOLLSEGNUM,TOLLSEGLEN,MINSEGTOLL,DISTANCE,SPEED,NUM_LANES,FTYPE,CAPACITY,DIR1
-FILEI LINKI[1] = "{Network}"
-ZONES = {Number of Zones}
+RUN PGM=NETWORK MSG='Final Daily Loaded Network'
+FILEI LINKI[4] = "{SCENARIO_DIR}\LOADED_NT.NET"
+FILEO NETO = "{SCENARIO_DIR}\LOADED_DY.NET",
+EXCLUDE=HRCAP,FFSPD,TOLL,TIME1,TIME2,LINKGRP1,LINKGRP2,LINKGRP3,ASGNGRP,USER,COST,TWOWAY,
+VOLUME,DIRCODE,VOLPURP1,CGSTDTIME,V_1,TIME_1,VC_1,VT_1,CSPD_1,VDT_1,VHT_1,V1_1,V2_1,V3_1,V4_1,V5_1,V6_1,V7_1,V8_1,V1T_1,V2T_1,V3T_1,V4T_1,V5T_1,
+V6T_1,V7T_1,V8T_1,LW_TOLL_1,LW_DUMMY_TIME_1,LW_CGTIME_1
+FILEI LINKI[3] = "{SCENARIO_DIR}\LOADED_MD.NET"
+FILEI LINKI[2] = "{SCENARIO_DIR}\LOADED_PM.NET"
+FILEI LINKI[1] = "{SCENARIO_DIR}\LOADED_AM.NET"
+
 MERGE RECORD=FALSE
+DY_SOV = LI.1.AM_SOV + LI.2.PM_SOV + LI.3.MD_SOV + LI.4.NT_SOV
+DY_HOV = LI.1.AM_HOV + LI.2.PM_HOV + LI.3.MD_HOV + LI.4.NT_HOV
+DY_TRK = LI.1.AM_TRK + LI.2.PM_TRK + LI.3.MD_TRK + LI.4.NT_TRK
 
-;Toll segment number
-IF(LI.2.TOLLSEGNUM>0)
-  TOLLSEGNUM = LI.2.TOLLSEGNUM
-ELSE
-  TOLLSEGNUM = 0
-ENDIF
+DY_TOT = DY_SOV + DY_HOV + DY_TRK
 
-;Toll segment length
-IF(LI.2.TOLLSEGLEN>0)
-  TOLLSEGLEN = LI.2.TOLLSEGLEN
-ENDIF
-
-;Minimum segment toll
-IF(LI.2.MINSEGTOLL>0)
-  MINSEGTOLL = LI.2.MINSEGTOLL
-ENDIF
-
-
-;Distance
-IF({Distance}=0)
-  DISTANCE=0.001
-ELSE
-  DISTANCE = {Distance};/100
-ENDIF
-
-;Lanes
-IF({Number of Lanes}=0)
-  NUM_LANES = 1 ;0
-ELSE
-  NUM_LANES = {Number of Lanes}
-ENDIF
-
-/*;Type
-IF(LI.3.Ftype=91)
-  FTYPE = 91
-ELSEIF(LI.1.Facility_type=0)
-  FTYPE = 80
-ELSEIF(LI.1.Facility_type=91)
-  FTYPE = 10
-ELSE
-  FTYPE = LI.1.Facility_type
-ENDIF
-*/
-
-;Type
-IF(LI.3.Ftype=91)
-  FTYPE = 91
-ELSEIF({Facility Type}=0)
-  FTYPE = 52
-ELSEIF({Facility Type}=90)
-  FTYPE = 10
-ELSE
-  FTYPE = {Facility Type}
-ENDIF
-
-;Free flow speed
-IF({Free Flow Speed}>0)
-  SPEED = {Free Flow Speed}
-ELSEIF(LI.1.TIME2=0)
-  SPEED = 999
-ELSE
-  SPEED = {Distance}/LI.1.TIME2*60
-ENDIF
-;postspd
-
-;Capacity
-IF({Capacity per Lane}=0)
-  CAPACITY = 9999
-ELSEIF(LI.4.HRCAP>0)
-  CAPACITY = LI.4.HRCAP
-ELSE
-  CAPACITY = {Capacity per Lane};/10/LI.1.NUM_LANES
-ENDIF
-;CAPACITY_lane
-
-;DIR1
-IF(LI.5.DIR1=1)
-  DIR1 = 1
-ENDIF
-
-/* === modified script that works on 7.16.16 ===
-;Toll segment number
-IF(LI.2.TOLLSEGNUM>0)
-  TOLLSEGNUM = LI.2.TOLLSEGNUM
-ELSE
-  TOLLSEGNUM = 0
-ENDIF
-
-;Toll segment length
-IF(LI.2.TOLLSEGLEN>0)
-  TOLLSEGLEN = LI.2.TOLLSEGLEN
-ENDIF
-
-;Minimum segment toll
-IF(LI.2.MINSEGTOLL>0)
-  MINSEGTOLL = LI.2.MINSEGTOLL
-ENDIF
-
-
-;Distance
-IF(LI.1.DISTANCE=0)
-  DISTANCE=0.001
-ELSE
-  DISTANCE = LI.1.DISTANCE;/100
-ENDIF
-
-;Lanes
-IF(LI.1.NUM_LANES=0)
-  NUM_LANES = 1 ;0
-ELSE
-  NUM_LANES = LI.1.NUM_LANES
-ENDIF
-*/
-/*;Type
-IF(LI.3.Ftype=91)
-  FTYPE = 91
-ELSEIF(LI.1.Facility_type=0)
-  FTYPE = 80
-ELSEIF(LI.1.Facility_type=91)
-  FTYPE = 10
-ELSE
-  FTYPE = LI.1.Facility_type
-ENDIF
-*/
-/*
-;Type
-IF(LI.3.Ftype=91)
-  FTYPE = 91
-ELSEIF(LI.1.FTC2=0)
-  FTYPE = 52
-ELSEIF(LI.1.FTC2=90)
-  FTYPE = 10
-ELSE
-  FTYPE = LI.1.FTC2
-ENDIF
-
-;Free flow speed
-IF(LI.1.speed>0)
-  SPEED = LI.1.speed
-ELSEIF(LI.1.TIME2=0)
-  SPEED = 999
-ELSE
-  SPEED = LI.1.DISTANCE/LI.1.TIME2*60
-ENDIF
-;postspd
-
-;Capacity
-IF(LI.1.CAPACITY=0)
-  CAPACITY = 9999
-ELSEIF(LI.4.HRCAP>0)
-  CAPACITY = LI.4.HRCAP
-ELSE
-  CAPACITY = LI.1.CAPACITY;/10/LI.1.NUM_LANES
-ENDIF
-;CAPACITY_lane
-
-;DIR1
-IF(LI.5.DIR1=1)
-  DIR1 = 1
-ENDIF
-*/
-
+;DY_TOT_SL = LI.1.AM_TOT_SL + LI.2.PM_TOT_SL + LI.3.MD_TOT_SL + LI.4.NT_TOT_SL
 ENDRUN
 
 
